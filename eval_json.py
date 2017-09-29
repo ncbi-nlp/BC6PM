0d6c90e4259907daea06cb1a3b9c67d9daa0aa5b
Sun Kim <sun.kim@nih.gov>
2017-09-29 15:15:59 -0400

    add average precision and other fixes

--- eval_json.py
+++ eval_json.py
@@ -5,8 +5,10 @@
 
 import json
 import sys
+import operator
 
 def JSON_Document(document):
+    confidence = 0
     id = document['id']
 
     if 'infons' not in document:
@@ -16,21 +18,29 @@ def JSON_Document(document):
         if 'relevant' in infons:
             label = infons['relevant']
             label = label.lower()
+            if 'confidence' in infons:
+                confidence = float(infons['confidence'])
         else:
             label = 'no'
+
+    if label != 'yes':
+        if confidence == 0:
+            confidence = -1
+        else:
+            confidence = -confidence
 
     if 'relations' not in document:
         relations = []
     else:
         relations = document['relations']
-    return id, label, relations
+    return id, label, confidence, relations
 
 def JSON_Collection_Triage(collection):
     positives = set()
     negatives = set()
 
     for document in collection.get('documents', []):
-        id, label, relations = JSON_Document(document)
+        id, label, confidence, relations = JSON_Document(document)
         if label == 'yes':
             positives.add(id)
         else:
@@ -41,25 +51,43 @@ def Classification_Performance_Triage(collection, gold_standard_positive, gold_s
     correct = prediction_count = 0
     precision = recall = f1 = 0
 
+    previously_seen = set()
+    prediction_dict = {}
+
     for document in collection.get('documents', []):
-        id, label, relations = JSON_Document(document)
-        if label == 'yes':
-            if id in gold_standard_positive:
-                correct += 1.
-            prediction_count += 1.
+        id, label, confidence, relations = JSON_Document(document)
+        if (id not in previously_seen) and (id in gold_standard_positive or id in gold_standard_negative):
+            if label == 'yes':
+                if id in gold_standard_positive:
+                    correct += 1.
+                prediction_count += 1.
+            prediction_dict[id] = confidence
+            previously_seen.add(id)
 
     if prediction_count > 0 and correct > 0 and len(gold_standard_positive) > 0:
         precision = correct / prediction_count
         recall = correct / len(gold_standard_positive)
         f1 = 2. * precision * recall / (precision + recall)
-    
-    return precision, recall, f1
+
+    correct = prediction_count = 0
+    average_precision = 0
+
+    prediction_dict = sorted(prediction_dict.items(), key=operator.itemgetter(1), reverse=True)
+    for id, confidence in prediction_dict:
+        prediction_count += 1.
+        if id in gold_standard_positive:
+            correct += 1.
+            average_precision += correct / prediction_count
+    average_precision /= len(gold_standard_positive)
+
+    return average_precision, precision, recall, f1
 
 def JSON_Collection_Relation(collection):
+    all_ids = set()
     all_relations = set()
  
     for document in collection.get('documents', []):
-        id, label, relations = JSON_Document(document)
+        id, label, confidence, relations = JSON_Document(document)
         for relation in relations:
             if 'infons' in relation:
                 relation_flag = 0
@@ -76,21 +104,26 @@ def JSON_Collection_Relation(collection):
                 if relation_flag == 1:
                     infon_values.sort()
                     relation_string = 'PMID' + id + '_' + '_'.join(infon_values)
+                    all_ids.add(id)
                     all_relations.add(relation_string)
 
-    return all_relations
+    return all_ids, all_relations
 
-def Classification_Performance_Relation(collection, gold_standard_relations):
+def Classification_Performance_Relation(collection, gold_standard_ids, gold_standard_relations):
     correct = prediction_count = 0
     precision = recall = f1 = 0
 
+    previously_seen = set()
+    prediction_dict = {}
+
     for document in collection.get('documents', []):
-        id, label, relations = JSON_Document(document)
-        for relation in relations:
-            if relation in relations:
+        id, label, confidence, relations = JSON_Document(document)
+        if id in gold_standard_ids:
+            for relation in relations:
                 if 'infons' in relation:
                     relation_flag = 0
                     infon_values = []
+                    relation_confidence = 0
 
                     infons = relation['infons']
                     for infon_type in infons:
@@ -99,13 +132,18 @@ def Classification_Performance_Relation(collection, gold_standard_relations):
                             relation_flag = 1
                         elif infon_type_lowercase[:4] == 'gene':
                             infon_values.append(infons[infon_type])
+                        elif infon_type_lowercase == 'confidence':
+                            relation_confidence = float(infons[infon_type])
             
                     if relation_flag == 1:
                         infon_values.sort()
                         relation_string = 'PMID' + id + '_' + '_'.join(infon_values)
-                        if relation_string in gold_standard_relations:
-                            correct += 1.
-                        prediction_count += 1.
+                        if relation_string not in previously_seen:
+                            if relation_string in gold_standard_relations:
+                                correct += 1.
+                            prediction_count += 1.
+                            prediction_dict[relation_string] = relation_confidence
+                            previously_seen.add(relation_string)
 
     if prediction_count > 0 and correct > 0 and len(gold_standard_relations) > 0:
         precision = correct / prediction_count
@@ -131,11 +169,16 @@ with open(gold_standard_file) as f1, open(prediction_file) as f2:
 
     if subtask == 'triage':
         gold_standard_positive, gold_standard_negative = JSON_Collection_Triage(gold_standard_collection)
-        precision, recall, f1 = Classification_Performance_Triage(prediction_collection, gold_standard_positive, gold_standard_negative)
-    else:
-        gold_standard_relations = JSON_Collection_Relation(gold_standard_collection)
-        precision, recall, f1 = Classification_Performance_Relation(prediction_collection, gold_standard_relations)
+        average_precision, precision, recall, f1 = Classification_Performance_Triage(prediction_collection, gold_standard_positive, gold_standard_negative)
 
-    print('Precision: %.4f' % precision)
-    print('Recall: %.4f' % recall)
-    print('F1: %.4f' % f1)
+        print('Avg Precision: %.4f' % average_precision)
+        print('Precision: %.4f' % precision)
+        print('Recall: %.4f' % recall)
+        print('F1: %.4f' % f1)        
+    else:
+        gold_standard_ids, gold_standard_relations = JSON_Collection_Relation(gold_standard_collection)
+        precision, recall, f1 = Classification_Performance_Relation(prediction_collection, gold_standard_ids, gold_standard_relations)
+
+        print('Precision: %.4f' % precision)
+        print('Recall: %.4f' % recall)
+        print('F1: %.4f' % f1)
--- eval_xml.py
+++ eval_xml.py
@@ -4,27 +4,41 @@
 ### Compute classification performance scores
 
 import sys
+import operator
 
 sys.path.append('/home/skim01/PyBioC/src') # Add the path to PyBioC src
 import bioc
 
 def BioC_Document(document):
+    confidence = 0
     id = document.id
 
     if len(document.infons) == 0:
         label = 'no'
     else:
-        label = document.infons['relevant']
-        label = label.lower()
+        if 'relevant' in document.infons:
+            label = document.infons['relevant']
+            label = label.lower()
+            if 'confidence' in document.infons:
+                if document.infons['confidence'] is not None:
+                    confidence = float(document.infons['confidence'])
+        else:
+            label = 'no'
 
-    return id, label, document.relations
+    if label != 'yes':
+        if confidence == 0:
+            confidence = -1
+        else:
+            confidence = -confidence
+
+    return id, label, confidence, document.relations
 
 def BioC_Collection_Triage(collection):
     positives = set()
     negatives = set()
 
     for document in collection.documents:
-        id, label, relations = BioC_Document(document)
+        id, label, confidence, relations = BioC_Document(document)
         if label == 'yes':
             positives.add(id)
         else:
@@ -35,25 +49,43 @@ def Classification_Performance_Triage(collection, gold_standard_positive, gold_s
     correct = prediction_count = 0
     precision = recall = f1 = 0
 
+    previously_seen = set()
+    prediction_dict = {}
+
     for document in collection.documents:
-        id, label, relations = BioC_Document(document)
-        if label == 'yes':
-            if id in gold_standard_positive:
-                correct += 1.
-            prediction_count += 1.
+        id, label, confidence, relations = BioC_Document(document)
+        if (id not in previously_seen) and (id in gold_standard_positive or id in gold_standard_negative):
+            if label == 'yes':
+                if id in gold_standard_positive:
+                    correct += 1.
+                prediction_count += 1.
+            prediction_dict[id] = confidence
+            previously_seen.add(id)
 
     if prediction_count > 0 and correct > 0 and len(gold_standard_positive) > 0:
         precision = correct / prediction_count
         recall = correct / len(gold_standard_positive)
         f1 = 2. * precision * recall / (precision + recall)
+
+    correct = prediction_count = 0
+    average_precision = 0
     
-    return precision, recall, f1
+    prediction_dict = sorted(prediction_dict.items(), key=operator.itemgetter(1), reverse=True)
+    for id, confidence in prediction_dict:
+        prediction_count += 1.
+        if id in gold_standard_positive:
+            correct += 1.
+            average_precision += correct / prediction_count
+    average_precision /= len(gold_standard_positive)
+
+    return average_precision, precision, recall, f1
 
 def BioC_Collection_Relation(collection):
+    all_ids = set()
     all_relations = set()
  
     for document in collection.documents:
-        id, label, relations = BioC_Document(document)
+        id, label, confidence, relations = BioC_Document(document)
         for relation in relations:
             relation_flag = 0
             infon_values = []
@@ -68,39 +100,51 @@ def BioC_Collection_Relation(collection):
             if relation_flag == 1:
                 infon_values.sort()
                 relation_string = 'PMID' + id + '_' + '_'.join(infon_values)
+                all_ids.add(id)
                 all_relations.add(relation_string)
 
-    return all_relations
+    return all_ids, all_relations
 
-def Classification_Performance_Relation(collection, gold_standard_relations):
+def Classification_Performance_Relation(collection, gold_standard_ids, gold_standard_relations):
     correct = prediction_count = 0
     precision = recall = f1 = 0
 
+    previously_seen = set()
+    prediction_dict = {}
+
     for document in collection.documents:
-        id, label, relations = BioC_Document(document)
-        for relation in relations:
-            relation_flag = 0
-            infon_values = []
+        id, label, confidence, relations = BioC_Document(document)
+        if id in gold_standard_ids:
+            for relation in relations:
+                relation_flag = 0
+                infon_values = []
+                relation_confidence = 0
 
-            for infon_type in relation.infons:
-                infon_type_lowercase = infon_type.lower()
-                if infon_type_lowercase == 'relation':
-                    relation_flag = 1
-                elif infon_type_lowercase[:4] == 'gene':
-                    infon_values.append(relation.infons[infon_type])
+                for infon_type in relation.infons:
+                    infon_type_lowercase = infon_type.lower()
+                    if infon_type_lowercase == 'relation':
+                        relation_flag = 1
+                    elif infon_type_lowercase[:4] == 'gene':
+                        infon_values.append(relation.infons[infon_type])
+                    elif infon_type_lowercase == 'confidence':
+                        if relation.infons[infon_type] is not None:
+                            relation_confidence = float(relation.infons[infon_type])
 
-            if relation_flag == 1:
-                infon_values.sort()
-                relation_string = 'PMID' + id + '_' + '_'.join(infon_values)
-                if relation_string in gold_standard_relations:
-                    correct += 1.
-                prediction_count += 1.
+                if relation_flag == 1:
+                    infon_values.sort()
+                    relation_string = 'PMID' + id + '_' + '_'.join(infon_values)
+                    if relation_string not in previously_seen:
+                        if relation_string in gold_standard_relations:
+                            correct += 1.
+                        prediction_count += 1.
+                        prediction_dict[relation_string] = relation_confidence
+                        previously_seen.add(relation_string)
 
     if prediction_count > 0 and correct > 0 and len(gold_standard_relations) > 0:
         precision = correct / prediction_count
         recall = correct / len(gold_standard_relations)
         f1 = 2. * precision * recall / (precision + recall)
-    
+ 
     return precision, recall, f1
 
 program_name = subtask = gold_standard_file = prediction_file = None
@@ -119,11 +163,16 @@ prediction_reader.read()
 
 if subtask == 'triage':
     gold_standard_positive, gold_standard_negative = BioC_Collection_Triage(gold_standard_reader.collection)
-    precision, recall, f1 = Classification_Performance_Triage(prediction_reader.collection, gold_standard_positive, gold_standard_negative)
-else:
-    gold_standard_relations = BioC_Collection_Relation(gold_standard_reader.collection)
-    precision, recall, f1 = Classification_Performance_Relation(prediction_reader.collection, gold_standard_relations)
+    average_precision, precision, recall, f1 = Classification_Performance_Triage(prediction_reader.collection, gold_standard_positive, gold_standard_negative)
 
-print('Precision: %.4f' % precision)
-print('Recall: %.4f' % recall)
-print('F1: %.4f' % f1)
+    print('Avg Precision: %.4f' % average_precision)
+    print('Precision: %.4f' % precision)
+    print('Recall: %.4f' % recall)
+    print('F1: %.4f' % f1)
+else:
+    gold_standard_ids, gold_standard_relations = BioC_Collection_Relation(gold_standard_reader.collection)
+    precision, recall, f1 = Classification_Performance_Relation(prediction_reader.collection, gold_standard_ids, gold_standard_relations)
+
+    print('Precision: %.4f' % precision)
+    print('Recall: %.4f' % recall)
+    print('F1: %.4f' % f1)
